2a463a031187743bf1658e769423d793e1a79e1f
Hex-long <steffensuhm@gmail.com>
2025-06-27 12:17:32 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
-// Firebase-Konfiguration kommt hier rein
+// js/firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBWlVw3NQjuEr77yQfNUWzpg-jb2K5YE6E",
+  authDomain: "gamehub-project-fb643.firebaseapp.com",
+  databaseURL: "https://gamehub-project-fb643-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gamehub-project-fb643",
+  storageBucket: "gamehub-project-fb643.appspot.com",
+  messagingSenderId: "38222999508",
+  appId: "1:38222999508:web:f1c907ce29c22afcc53041"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
